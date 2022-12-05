0aa607c4d4effb6b2371dbfbf8ce7bb9e97e6681
Dmitry Pavlov <dimapav1985@mail.ru>
2022-12-06 00:57:01 +0700

    fix: fixed to-ref

--- src/components/PageNotFound/PageNotFound.jsx
+++ src/components/PageNotFound/PageNotFound.jsx
@@ -1,15 +1,22 @@
-import { useHistory } from "react-router";
+import { Link, useHistory } from "react-router-dom";
 import "./PageNotFound.css";
 
 const PageNotFound = () => {
   const history = useHistory();
   const goBack = () => history.go(-1);
+  const backRef = () => history.createHref(goBack);
 
   return (
     <section className="not-found">
       <h2 className="not-found__title">404</h2>
       <p className="not-found__text">Страница не найдена</p>
-      <button className="not-found__link" onClick={goBack} >Назад</button>
+      <Link
+        className="not-found__link"
+        onClick={goBack}
+        to={backRef}
+      >
+        Назад
+      </Link>
     </section>
   );
 };
